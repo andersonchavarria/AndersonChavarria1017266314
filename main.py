122b52589e396fe6a0ecc10008e8329a3b4dd3c1
andersonchavarria <andersonchavarria323355@correo.itm.edu.co>
2024-11-29 16:07:50 -0500

    Update print
    cambiamos el hola mundo a uno mas largo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hola, mundo")
+    print("Hola, mundo, En este momento generamos un update el este archivo con el fin de obtener un cambios para la entrega final del  la materia servicios web")
  
 if __name__ == "__main__":
     main()
